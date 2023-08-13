105b32c9e48a0f3ce643c25957e3dfd4df0b91a0
asil004 <72306088+asil004@users.noreply.github.com>
2023-08-13 16:32:08 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    await message.answer(f'Assalomu aleykum {message.from_user.first_name}.\n'
+                         f'Online krasovkalar do\'koniga xush kelibsiz!')
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply('Sizni tushunmayabman!')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        executor.start_polling(dp)
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("Bot stopped!")
